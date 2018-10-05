72516ca6d6e0fb2d6dfa9d0c3fd1c443c820df3e
Łukasz Nawrat <lnawrat@future-processing.com>
2018-10-05 16:53:37 +0200

    2018-Q4-1: Added swap command.

--- Scripts/Swap-WebAppSlot.ps1
+++ Scripts/Swap-WebAppSlot.ps1
@@ -7,3 +7,13 @@ Select-AzurermSubscription -SubscriptionId {Your-sub-guid}
 Write-Output "[Swap slots] Subscription selected successfully."
 
 Write-Output "[Swap slots] Swap slot."
+
+$parameters = @{ targetSlot = "stage" };
+Invoke-AzureRmResourceAction `
+    -ResourceGroupName {your_name} `
+    -ResourceType Microsoft.Web/sites `
+    -ResourceName {your_name} `
+    -Action slotsswap `
+    -Parameters $parameters `
+    -ApiVersion 2018-02-01 `
+    -Force
