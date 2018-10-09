73badbbd9ef6bfd446e684f2d6f2bed882e09914
Łukasz Nawrat <lnawrat@future-processing.com>
2018-10-09 21:07:01 +0200

    2018-Q4-1: Added Service bus demo.

--- AzureConstructionsProgressTracker/Global.asax.cs
+++ AzureConstructionsProgressTracker/Global.asax.cs
@@ -1,6 +1,9 @@
 ﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Common;
+
+using static System.Configuration.ConfigurationManager;
 
 namespace AzureConstructionsProgressTracker
 {
@@ -12,6 +15,9 @@ namespace AzureConstructionsProgressTracker
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            new ProjectAddedQueueService(ConnectionStrings["AzureServiceBus"].ConnectionString)
+                .Register();
         }
     }
 }
---
+++ Common/ProjectAddedQueueService.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Common
+{
+    public class ProjectAddedQueueService
+    {
+        private const string QueueName = "projectadded";
+        private readonly QueueClient _queueClient;
+        private readonly ConstructionsProgressTrackerContext _context;
+
+        public ProjectAddedQueueService(string serviceBusConnectionString)
+        {
+            _queueClient = QueueClient.CreateFromConnectionString(serviceBusConnectionString, QueueName);
+            _context = new ConstructionsProgressTrackerContext();
+        }
+
+        public void Register()
+        {
+            _queueClient.OnMessageAsync(async message =>
+            {
+                await ProcessMessagesAsync(message);
+
+                await message.CompleteAsync();
+            },
+            new OnMessageOptions
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            });
+        }
+
+        private async Task ProcessMessagesAsync(BrokeredMessage message)
+        {
+            var messageBodyStream = message.GetBody<Stream>();
+            string messageBody;
+            using (TextReader reader = new StreamReader(messageBodyStream, Encoding.UTF8))
+            {
+                messageBody = await reader.ReadToEndAsync();
+            }
+
+            _context.ConstructionProjects.Add(new ConstructionProject
+            {
+                Name = messageBody,
+                Notes = "From Service Bus message."
+            });
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
